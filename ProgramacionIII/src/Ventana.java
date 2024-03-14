2feb04bf47fdbe723a39791757446f8034376211
HenryC31 <159938672+HenryC31@users.noreply.github.com>
2024-03-14 07:45:31 -0700

    Unidad 3 Ejercicio 11
    Unidad 3 Ejercicio 11

--- ProgramacionIII/src/Ventana.java
+++ ProgramacionIII/src/Ventana.java
@@ -118,7 +118,11 @@ public class Ventana extends JFrame {
 
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
+				int r = (int) Math.floor(Math.random() * 255 + 1);
+				int g = (int) Math.floor(Math.random() * 255 + 1);
+				int b = (int) Math.floor(Math.random() * 255 + 1);
+
+				panel_btn.setBackground(new Color(r, g, b));
 
 			}
 
